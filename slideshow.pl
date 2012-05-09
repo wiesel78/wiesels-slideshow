3347dc98b622db8a9509568f8091feb630e3b462
wiesel78 <wurst78@googlemail.com>
2012-05-09 21:35:03 +0200

    vorerst fertig

--- slideshow.pl
+++ slideshow.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 
 use v5.14;
 use Getopt::Long;
@@ -7,17 +7,20 @@ use Getopt::Long;
 # -v, --verbose                 => show XML on STDOUT
 # -t, --time (seconds)          => time to next change
 # -f, --file (path/xml-file)    => create xml on path/xml-file
+# -s, --set-background          => set $file as background (gnome 3)
 
 
-my (@dateien, $duration, $verbose, $file, $slideshow);
+my (@dateien, $duration, $verbose, $file, $slideshow, $set_background);
 
 Getopt::Long::Configure('bundling');
-GetOptions( 'time:i'    => \$duration,
-            't:i'       => \$duration,
-            'verbose'   => \$verbose,
-            'v'         => \$verbose,
-            'file:s'    => \$file,
-            'f:s'       => \$file
+GetOptions( 'time:i'            => \$duration,
+            't:i'               => \$duration,
+            'verbose'           => \$verbose,
+            'v'                 => \$verbose,
+            'file:s'            => \$file,
+            'f:s'               => \$file,
+            'set-background'    => \$set_background,
+            's'                 => \$set_background
             ) or die ('Fehler bei den Parameter');
             
 
@@ -26,16 +29,29 @@ $duration       or $duration = 300;
 $slideshow      = createSlideshow(@dateien, $duration);
 $verbose        and say $slideshow;
 
-$file and 
-createXMLFile($slideshow, $file) or 
-$file = "$ENV{HOME}/slideshow2.xml";
+if($file)
+{ 
+    createXMLFile($slideshow, $file); 
+}else
+{
+    $file = "$ENV{HOME}/slideshow.xml";
+    createXMLFile($slideshow, $file) if $set_background;
+}
 
+$set_background and setBackground($file);
+
+
+sub setBackground
+{
+    my $file = shift @_;
+    `gsettings set org.gnome.desktop.background picture-uri "file://$file"`;
+}
 
 sub createXMLFile
 {
     my ($slideshow, $file) = @_;
     
-    open (DATEI, ">> $file") or die ('Fehler beim Schreiben');
+    open (DATEI, "> $file") or die ('Fehler beim Schreiben');
     print DATEI "$slideshow";
     close DATEI;
     
@@ -97,13 +113,15 @@ sub createSlideshowHeader
 
 sub createSlideshowBody
 {
+    my $typ;
     my $xml         = "";
     my $duration    = pop @_ ;
     my @pictures    = @_ ;
     
-    foreach (@pictures)
+    foreach my $file (@pictures)
     {
-        $xml .= createSlideshowElement($_, $duration);
+        $_ = `file --mime-type "$file"`;
+        $xml .= createSlideshowElement($file, $duration) if /:\s(image)\/.*/;
     }
     
     return $xml;
